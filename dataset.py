168778429d7f7c13a9bf54370c88101da98ec847
Vladimir Iglovikov <iglovikov@gmail.com>
2018-04-15 18:04:13 -0700

    Cleaned dataset generator. Only binary case left

--- dataset.py
+++ dataset.py
@@ -3,18 +3,16 @@ import numpy as np
 import cv2
 from torch.utils.data import Dataset
 from pathlib import Path
-import prepare_data
 
 data_path = Path('data')
 
 
-class RoboticsDataset(Dataset):
-    def __init__(self, file_names: list, to_augment=False, transform=None, mode='train', problem_type=None):
+class AngyodysplasiaDataset(Dataset):
+    def __init__(self, file_names: list, to_augment=False, transform=None, mode='train'):
         self.file_names = file_names
         self.to_augment = to_augment
         self.transform = transform
         self.mode = mode
-        self.problem_type = problem_type
 
     def __len__(self):
         return len(self.file_names)
@@ -22,15 +20,13 @@ class RoboticsDataset(Dataset):
     def __getitem__(self, idx):
         img_file_name = self.file_names[idx]
         img = load_image(img_file_name)
-        mask = load_mask(img_file_name, self.problem_type)
+        mask = load_mask(img_file_name)
 
         img, mask = self.transform(img, mask)
 
         if self.mode == 'train':
-            if self.problem_type == 'binary':
-                return to_float_tensor(img), torch.from_numpy(np.expand_dims(mask, 0)).float()
-            else:
-                return to_float_tensor(img), torch.from_numpy(mask).long()
+            return to_float_tensor(img), torch.from_numpy(np.expand_dims(mask, 0)).float()
+
         else:
             return to_float_tensor(img), str(img_file_name)
 
@@ -44,17 +40,6 @@ def load_image(path):
     return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 
-def load_mask(path, problem_type):
-    if problem_type == 'binary':
-        mask_folder = 'binary_masks'
-        factor = prepare_data.binary_factor
-    elif problem_type == 'parts':
-        mask_folder = 'parts_masks'
-        factor = prepare_data.parts_factor
-    elif problem_type == 'instruments':
-        factor = prepare_data.instrument_factor
-        mask_folder = 'instruments_masks'
-
-    mask = cv2.imread(str(path).replace('images', mask_folder).replace('jpg', 'png'), 0)
-
-    return (mask / factor).astype(np.uint8)
+def load_mask(path):
+    mask = cv2.imread(str(path).replace('images', 'masks').replace(r'.jpg', r'_a.jpg'), 0)
+    return (mask > 0).astype(np.uint8)
